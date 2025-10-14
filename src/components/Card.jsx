5141dabddc13bc48d56231675e1c0c62c323f6e3
JesseChum <jessechum@icloud.com>
2025-10-14 13:31:27 -0700

    hardcoded first card

---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+export default function Card(){
+    return(
+        <div>
+        <h1>title</h1>
+        <p>genre</p>
+        <p>rating</p>
+        <p>director</p>
+        <p>actors</p>
+        </div>
+    )
+}
